89a1415bb55a7d5e9d57c615b355bb94c24246f6
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-03-30 21:55:01 +0300

    4

---
+++ src/Window.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Dmitrii on 30.03.2017.
+ */
+public class Window {
+    JTextArea textArea;
+    JTextArea ta;
+    public Window() {
+        JFrame frame = new JFrame("YouXOR");
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new BorderLayout());
+
+        textArea = new JTextArea("Введите текст", 4, 20);
+        textArea.setBackground(Color.white);
+        panel.add("West",textArea);
+
+        ta = new JTextArea(4,20);
+        ta.setBackground(Color.white);
+        ta.setEditable(false);
+        panel.add("East",ta);
+
+        JButton button = new JButton("Шифровать");
+        button.addActionListener(new Action());
+        panel.add("Center", button);
+
+        JTextField key = new JTextField("KEY", 4);
+        panel.add("South", key);
+
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setResizable(false);
+    }
+    class Action implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            ta.setText(textArea.getText());
+
+        }
+    }
+}
