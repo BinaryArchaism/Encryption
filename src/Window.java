a92c10d8f19f35d9689499bde86d9c68d2aee409
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-04-01 12:03:43 +0300

    v3.0

--- src/Window.java
+++ src/Window.java
@@ -43,26 +43,21 @@ public class Window {
         frame.pack();
         frame.setResizable(false);
     }
-    class Action implements ActionHandler {
+    class Action implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e) throws UnsupportedEncodingException  {
-            byte[] arr = toByte(textArea.getText());
+        public void actionPerformed(ActionEvent e) {
 
-        }
-        public byte[] toByte(String s) throws UnsupportedEncodingException {
-            byte[] arr = s.getBytes("UTF-8");
-            return arr;
-        }
-        public String toStr(byte[] arr) throws UnsupportedEncodingException {
-            String s = new String(arr, "UTF-8");
-            return s;
+            byte[] k = (key.getText().getBytes());
+            byte[] arr = (textArea.getText().getBytes());
+            int n = arr.length;
+            for (int i = 0; i <= n; i++) {
+                arr[i] = (byte) (arr[i] ^ k[i]);
+            }
+            ta.setText(bytesToBin(arr));
         }
         public String byteToBin(byte b){
             return String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
         }
-        /*
-         *  Переводим массив byte в строку с двоичным видом
-         */
         public String bytesToBin(byte[] bytes){
             StringBuilder stringBuilder = new StringBuilder();
             for(byte b:bytes){
